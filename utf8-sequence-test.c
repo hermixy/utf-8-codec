7239aeab1844552066755ce9d08ed343f1986ad5
Taylor Holberton <taylorcholberton@gmail.com>
2015-03-15 16:26:57 -0700

    added Init and Done test

--- utf8-sequence-test.c
+++ utf8-sequence-test.c
@@ -20,8 +20,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int utf8_sequence__Test_Init(void);
+
+int utf8_sequence__Test_Done(void);
+
+int utf8_sequence__Test_Encode(void);
+
+int utf8_sequence__Test_Decode(void);
+
 int main(void){
 
+	if (utf8_sequence__Test_Init() < 0){
+		fprintf(stderr, "Init test failed\n");
+		return EXIT_FAILURE;
+	}
+
+	if (utf8_sequence__Test_Done() < 0){
+		fprintf(stderr, "Done test failed\n");
+		return EXIT_FAILURE;
+	}
+
+	if (utf8_sequence__Test_Encode() < 0){
+		fprintf(stderr, "Encode test failed\n");
+		return EXIT_FAILURE;
+	}
+
+	if (utf8_sequence__Test_Decode() < 0){
+		fprintf(stderr, "Decode test failed\n");
+		return EXIT_FAILURE;
+	}
+
 	return EXIT_SUCCESS;
+}
+
+int utf8_sequence__Test_Init(void){
+
+	int i = 0;
+
+	utf8_sequence sequence;
+
+	if (utf8_sequence_Init(&sequence) < 0){
+		fprintf(stderr, "failed to initialize sequence\n");
+		return -1;
+	}
+
+	if (sequence.point_count != 0){
+		fprintf(stderr, "point count != 0 after initialization\n");
+		return -2;
+	}
+
+	for (i = 0; i < UTF8_SEQUENCE_POINT_MAX; i++){
+		if (sequence.point_array[i] != 0){
+			fprintf(stderr, "point[%d] != 0\n", i);
+			return -3;
+		}
+	}
+
+	return 0;
+}
+
+int utf8_sequence__Test_Done(void){
+
+	int i = 0;
+
+	utf8_sequence sequence;
+
+	if (utf8_sequence_Init(&sequence) < 0){
+		fprintf(stderr, "failed to initialize sequence\n");
+		return -1;
+	}
+
+	for (i = 0; i < UTF8_SEQUENCE_POINT_MAX; i++){
+		sequence.point_array[i] = 0xF0;
+	}
+
+	utf8_sequence_Done(&sequence);
+
+	if (sequence.point_count != 0){
+		fprintf(stderr, "point count != 0 after utf8_sequence_Done\n");
+		return -2;
+	}
+
+	for (i = 0; i < UTF8_SEQUENCE_POINT_MAX; i++){
+		if (sequence.point_array[i] != 0){
+			fprintf(stderr, "point[%d] != 0\n", i);
+			return -3;
+		}
+	}
+
+	return 0;
+}
+
+int utf8_sequence__Test_Encode(void){
+	return 0;
+}
+
+int utf8_sequence__Test_Decode(void){
+	return 0;
 }
 
