73210da28dbdc0861bb739ecda59f8bcd8b64512
Taylor Holberton <taylorcholberton@gmail.com>
2015-03-15 15:40:32 -0700

    added utf8-sequence-test

---
+++ utf8-sequence-test.c
@@ -0,0 +1,27 @@
+/*
+ *    This file is part of Utf8String.
+ *
+ *    Utf8String is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Utf8String is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with Utf8String.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "utf8-sequence.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+	return EXIT_SUCCESS;
+}
+
