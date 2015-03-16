183d297f167dafd63c46d4968d4e86a51c4430b0
Taylor Holberton <taylorcholberton@gmail.com>
2015-03-16 12:42:59 -0700

    completed encoding function

--- utf8-sequence.c
+++ utf8-sequence.c
@@ -54,9 +54,64 @@ utf8_char utf8_sequence_Decode(utf8_sequence * sequence){
 		}
 
 		c <<= point_bit_count;
-		c  += point_data;
+		c += point_data;
 	}
 
 	return c;
+}
+
+int utf8_sequence_Encode(utf8_sequence * sequence, utf8_char c){
+
+	memset(sequence, 0, sizeof(*sequence));
+
+	if (c <= 0x7F){
+
+		sequence->point_array[0] = (utf8_point) (c & 0x7F);
+		return 1;
+
+	} else if (c <= 0x07FF){
+
+		sequence->point_array[0] = (utf8_point) (((c >> 0x06) & 0x1F) | 0xC0);
+		sequence->point_array[1] = (utf8_point) (((c >> 0x00) & 0x3F) | 0x80);
+		return 2;
+
+	} else if (c <= 0xFFFF){
+
+		sequence->point_array[0] = (utf8_point) (((c >> 0x0C) & 0x0F) | 0xE0);
+		sequence->point_array[1] = (utf8_point) (((c >> 0x06) & 0x3F) | 0x80);
+		sequence->point_array[2] = (utf8_point) (((c >> 0x00) & 0x3F) | 0x80);
+		return 3;
+
+	} else if (c <= 0x1FFFFF){
+
+		sequence->point_array[0] = (utf8_point) (((c >> 0x12) & 0x07) | 0xF0);
+		sequence->point_array[1] = (utf8_point) (((c >> 0x0C) & 0x3F) | 0x80);
+		sequence->point_array[2] = (utf8_point) (((c >> 0x06) & 0x3F) | 0x80);
+		sequence->point_array[3] = (utf8_point) (((c >> 0x00) & 0x3F) | 0x80);
+		return 4;
+
+	} else if (c <= 0x3FFFFFF){
+
+		sequence->point_array[0] = (utf8_point) (((c >> 0x18) & 0x03) | 0xF8);
+		sequence->point_array[1] = (utf8_point) (((c >> 0x12) & 0x3F) | 0x80);
+		sequence->point_array[2] = (utf8_point) (((c >> 0x0C) & 0x3F) | 0x80);
+		sequence->point_array[3] = (utf8_point) (((c >> 0x06) & 0x3F) | 0x80);
+		sequence->point_array[4] = (utf8_point) (((c >> 0x00) & 0x3F) | 0x80);
+		return 5;
+
+	} else if (c <= 0x7FFFFFFF){
+
+		sequence->point_array[0] = (utf8_point) (((c >> 0x1E) & 0x01) | 0xFC);
+		sequence->point_array[1] = (utf8_point) (((c >> 0x18) & 0x3F) | 0x80);
+		sequence->point_array[2] = (utf8_point) (((c >> 0x12) & 0x3F) | 0x80);
+		sequence->point_array[3] = (utf8_point) (((c >> 0x0C) & 0x3F) | 0x80);
+		sequence->point_array[4] = (utf8_point) (((c >> 0x06) & 0x3F) | 0x80);
+		sequence->point_array[5] = (utf8_point) (((c >> 0x00) & 0x3F) | 0x80);
+		return 6;
+	}
+
+	/* should be unreachable, but would
+	 * otherwise indicate an invalid range */
+	return -1;
 }
 
